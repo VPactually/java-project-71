1262139b5e459ac0d68fe1a4761f2afb56d6d2c2
Vladislav Pomozov <vpactually@gmail.com>
2024-01-05 10:53:16 +0300

    add stylish formater

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -4,6 +4,10 @@ import java.util.Map;
 import java.util.TreeSet;
 
 public class Differ {
+    public static StringBuilder stylishFormat(StringBuilder sb, String key, String symbol, Object value) {
+        return sb.append("  ").append(symbol).append(' ').append(key).append(": ").append(value).append("\n");
+    }
+
     public static String generate(Map<String, Object> data1, Map<String, Object> data2) {
         StringBuilder sb = new StringBuilder();
         var set = new TreeSet<>(data1.keySet());
@@ -11,14 +15,14 @@ public class Differ {
         sb.append("{\n");
         for (var key : set) {
             if (!data1.containsKey(key)) {
-                sb.append("  + ").append(key).append(": ").append(data2.get(key)).append("\n");
+                sb = stylishFormat(sb, key, "+", data2.get(key));
             } else if (!data2.containsKey(key)) {
-                sb.append("  - ").append(key).append(": ").append(data1.get(key)).append("\n");
+                sb = stylishFormat(sb, key, "-", data1.get(key));
             } else if (data1.get(key) == null || !data1.get(key).equals(data2.get(key))) {
-                sb.append("  - ").append(key).append(": ").append(data1.get(key)).append("\n");
-                sb.append("  + ").append(key).append(": ").append(data2.get(key)).append("\n");
-            } else  {
-                sb.append("    ").append(key).append(": ").append(data1.get(key)).append("\n");
+                sb = stylishFormat(sb, key, "-", data1.get(key));
+                sb = stylishFormat(sb, key, "+", data2.get(key));
+            } else {
+                sb = stylishFormat(sb, key, " ", data1.get(key));
             }
         }
         sb.append("}");
