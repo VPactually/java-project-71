52a01c50b53a79e715b1763135bd54b85899dd6f
Vladislav Pomozov <vpactually@gmail.com>
2024-01-05 11:19:50 +0300

    refactor stylish format

--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,30 +1,33 @@
 package hexlet.code;
 
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeSet;
 
 public class Differ {
-    public static StringBuilder stylishFormat(StringBuilder sb, String key, String symbol, Object value) {
-        return sb.append("  ").append(symbol).append(' ').append(key).append(": ").append(value).append("\n");
+    private static void stylishFormat(StringBuilder sb, String key, String indicator, Object value) {
+        sb.append(String.format("  %s %s: %s\n", indicator, key, value));
     }
 
     public static String generate(Map<String, Object> data1, Map<String, Object> data2) {
         StringBuilder sb = new StringBuilder();
-        var set = new TreeSet<>(data1.keySet());
-        set.addAll(data2.keySet());
+        Set<String> keys = new TreeSet<>(data1.keySet());
+        keys.addAll(data2.keySet());
         sb.append("{\n");
-        for (var key : set) {
+        keys.forEach(key -> {
+            Object value1 = data1.get(key);
+            Object value2 = data2.get(key);
             if (!data1.containsKey(key)) {
-                sb = stylishFormat(sb, key, "+", data2.get(key));
+                stylishFormat(sb, key, "+", value2);
             } else if (!data2.containsKey(key)) {
-                sb = stylishFormat(sb, key, "-", data1.get(key));
-            } else if (data1.get(key) == null || !data1.get(key).equals(data2.get(key))) {
-                sb = stylishFormat(sb, key, "-", data1.get(key));
-                sb = stylishFormat(sb, key, "+", data2.get(key));
+                stylishFormat(sb, key, "-", value1);
+            } else if (value1 == null || !value1.equals(value2)) {
+                stylishFormat(sb, key, "-", value1);
+                stylishFormat(sb, key, "+", value2);
             } else {
-                sb = stylishFormat(sb, key, " ", data1.get(key));
+                stylishFormat(sb, key, " ", value1);
             }
-        }
+        });
         sb.append("}");
         return sb.toString();
     }
